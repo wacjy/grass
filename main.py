4376238d9e8a68781cae1800fd354e57fff0e63d
wacjy <106176216+wacjy@users.noreply.github.com>
2025-02-14 16:23:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ ONETIME_PROXY = get_proxy_count()
 DELAY_INTERVAL = 0.5
 MAX_RETRIES = 3
 FILE_UID = "/content/grass/uid.txt"
-FILE_PROXY = "/content/grass/proxy.txtt"
+FILE_PROXY = "/content/grass/proxy.txt"
 USERAGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.2365.57",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.2365.52",
